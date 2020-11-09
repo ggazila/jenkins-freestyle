17faa62ee58fe99d4963acb6e55fb27fcfd4aeef
Serhii Yevtushok <sergey.yevtushok@nixsolutions.com>
2020-11-09 12:31:11 +0200

    fix mapping

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,10 +3,10 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/home")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/admin")
     public String adminHome() {
         return "admin home";
     }
