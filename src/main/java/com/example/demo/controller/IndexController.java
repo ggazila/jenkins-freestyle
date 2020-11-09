b8e0056298e70c05957bd4414a15ce690401f4d2
Serhii Yevtushok <sergey.yevtushok@nixsolutions.com>
2020-11-09 17:25:35 +0200

    refactoring

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,11 +3,21 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/")
 public class IndexController {
 
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/admin")
+    public String adminHome() {
+        return "ADMIN HOME";
+    }
+
+    @GetMapping("/user")
+    public String userHome() {
+        return "user home";
     }
 }
